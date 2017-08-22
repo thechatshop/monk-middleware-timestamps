7592f59da8b1858e3b774e3cbcdf0eacaeb5d8ad
George Aidonidis <george.aidonidis@gmail.com>
2017-08-22 14:53:07 +0300

    Return promises on test
    * Return promises (like db.create) on mocha hooks.
    * Return promises from inside the `it` functions.
    * Change node version on travis.

--- test/index.js
+++ test/index.js
@@ -11,15 +11,15 @@ db.addMiddleware(addTimestamps);
 describe('Middleware timestamps', () => {
 	let dummyCollection;
 	before(() => {
-		db.create('dummy');
-		dummyCollection = db.get('dummy');
+		return db.create('dummy');
 	});
 	after(() => {
-		dummyCollection.drop();
+		return dummyCollection.drop();
 	});
 	it('insert method adds `createdAt` property if not exists', () => {
 		// Test an object
-		dummyCollection.insert({some: 'object'})
+		dummyCollection = db.get('dummy');
+		return dummyCollection.insert({some: 'object'})
 			.then(doc => {
 				expect(doc).to.have.property('createdAt');
 				expect(doc.createdAt).to.be.a('Date');
@@ -27,8 +27,10 @@ describe('Middleware timestamps', () => {
 				expect(doc.some).to.equal('object');
 			})
 			.catch(console.error);
+	});
+	it('insert method adds `createdAt` property on array', () => {
 		// Test an array of objects
-		dummyCollection.insert([{another: 'object'}, {yet: 'another'}])
+		return dummyCollection.insert([{another: 'object'}, {yet: 'another'}])
 			.then(docs => {
 				docs.forEach(doc => {
 					expect(doc).to.have.property('createdAt');
@@ -38,7 +40,7 @@ describe('Middleware timestamps', () => {
 			.catch(console.error);
 	});
 	it('update method updates/adds `updatedAt`', () => {
-		dummyCollection.update({some: 'object'}, {some: 'changedObject'})
+		return dummyCollection.update({some: 'object'}, {some: 'changedObject'})
 			.then(() => {
 				dummyCollection.findOne({some: 'changedObject'})
 					.then(doc => {
@@ -51,7 +53,7 @@ describe('Middleware timestamps', () => {
 			.catch(console.error);
 	});
 	it('findOneAndUpdate method updates/adds `updatedAt`', () => {
-		dummyCollection.findOneAndUpdate({another: 'object'}, {another: 'changedHere'})
+		return dummyCollection.findOneAndUpdate({another: 'object'}, {another: 'changedHere'})
 			.then(doc => {
 				expect(doc).to.be.an('object');
 				expect(doc).to.have.property('updatedAt');
